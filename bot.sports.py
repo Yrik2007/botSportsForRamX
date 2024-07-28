a69f91d9717e016d2d9dcda9dec4ec2e0ac2b384
RodyGroup <99239514+Yrik2007@users.noreply.github.com>
2024-07-28 22:31:57 +0300

    Update bot.sports.py

--- bot.sports.py
+++ bot.sports.py
@@ -1,13 +1,10 @@
 import time
 import telebot
 import feedparser
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from threading import Timer
 import json
 import os
-import random
 
 # Ваш API ключ для Telegram
 TELEGRAM_BOT_TOKEN = '6618460537:AAFNc6-KewtnwuH_9KktxcvD7fVZRIjh4FE'
@@ -23,19 +20,6 @@ LAST_SENT_FILE = 'last_sent.json'
 
 # Очередь для хранения новых новостей
 news_queue = []
-
-# Заголовки для имитации запроса от веб-браузера
-USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-]
-
-def get_random_headers():
-    """Возвращает случайные заголовки для имитации различных браузеров"""
-    return {'User-Agent': random.choice(USER_AGENTS)}
 
 def save_last_sent_time(last_sent_time):
     """Сохранение временной метки последней отправленной новости в файл"""
@@ -54,32 +38,6 @@ def load_last_sent_time():
 # Инициализация временной метки последней отправленной новости
 last_sent_time = load_last_sent_time()
 
-def get_html_content(url, retries=5, base_delay=2):
-    """Получение HTML контента по URL с фиксированной задержкой и повторными попытками"""
-    delay = base_delay
-    for attempt in range(retries):
-        try:
-            headers = get_random_headers()
-            response = requests.get(url, headers=headers)
-            if response.status_code == 429:
-                print(f"Rate limit exceeded for URL {url}. Retrying in {delay} seconds...")
-                time.sleep(delay)
-                delay *= 2  # Увеличиваем задержку при повторных попытках
-            else:
-                response.raise_for_status()
-                return response.text
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching the URL {url}: {e}. Retrying...")
-            time.sleep(delay)
-            delay *= 2  # Увеличиваем задержку при повторных попытках
-    return None
-
-def extract_image_url(html_content, class_name):
-    """Извлечение URL изображения из HTML контента"""
-    soup = BeautifulSoup(html_content, 'html.parser')
-    img = soup.find('img', class_=class_name)
-    return img['src'] if img else None
-
 def get_latest_news():
     """Получение последних новостей из RSS-ленты"""
     feed = feedparser.parse(RSS_FEED_URL)
@@ -87,17 +45,11 @@ def get_latest_news():
     for entry in reversed(feed.entries[:10]):  # Получаем только последние 10 новостей
         published_time = datetime(*entry.published_parsed[:6])
         if published_time > last_sent_time:
-            url = entry.link
-            html_content = get_html_content(url)
-            if html_content:
-                image_url = extract_image_url(html_content, 'af30e1399f')
-                articles.append({
-                    'title': entry.title,
-                    'link': entry.link,
-                    'image_url': image_url,
-                    'description': entry.summary,
-                    'published_time': published_time
-                })
+            articles.append({
+                'title': entry.title,
+                'description': entry.summary,
+                'published_time': published_time
+            })
     return articles
 
 def check_for_new_articles():
@@ -119,19 +71,11 @@ def send_news_from_queue():
         caption = f"{article['title']}\n\n{article['description']}"
 
         try:
-            if article['image_url']:
-                bot.send_photo(
-                    chat_id=-1002174719581,
-                    photo=article['image_url'],
-                    caption=caption,
-                    parse_mode='Markdown'
-                )
-            else:
-                bot.send_message(
-                    chat_id=-1002174719581,
-                    text=caption,
-                    parse_mode='Markdown'
-                )
+            bot.send_message(
+                chat_id=-1002174719581,
+                text=caption,
+                parse_mode='Markdown'
+            )
             print(f"Sent article: {article['title']}")
         except Exception as e:
             print(f"Error sending message: {e}")
